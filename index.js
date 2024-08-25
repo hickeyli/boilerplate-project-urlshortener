d9b1da194d89283c9e04f2751823ff0a15ffc420
hickeyli <hickeyli@msu.edu>
2024-08-25 14:39:34 -0400

    test23

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
 app.post('/api/shorturl', (req, res) => {
   const short_url = req.params.short_url;
   const url = urls[short_url];
@@ -18,19 +34,6 @@ app.post('/api/shorturl', (req, res) => {
     }
   }
 );
-
-app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
 
 
 
