284942ce46d5d7cd39481e9e01b872ef4d15355d
hickeyli <hickeyli@msu.edu>
2024-08-25 14:36:31 -0400

    change

--- index.js
+++ index.js
@@ -7,19 +7,6 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 app.get('/api/shorturl', function(req, res) {
   const { url } = req.query;
 
@@ -42,6 +29,21 @@ app.get('/api/shorturl', function(req, res) {
     res.json({ error: 'Invalid URL' });
   }
 });
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
